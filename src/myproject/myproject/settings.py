4901b2bf1e3a903d2322ef5fd31d4f335246e141
Mieda0310 <miedas@colabmix.co.jp>
2024-05-26 18:34:24 +0900

    novelのindex作った

--- src/myproject/myproject/settings.py
+++ src/myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +44,9 @@ INSTALLED_APPS = [
 
     'django.contrib.sites',
     'allauth',
-    'allauth.account'
+    'allauth.account',
+
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +66,10 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            os.path.join(BASE_DIR, "config", 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- src/myproject/text_app/views.py
+++ src/myproject/text_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import *
 
 class IndexNovel(View):
+    template_name = 'novel/index.html'
     def get(self, request, *args, **kwargs):
-        template_name = 'novel/index.html'
-        return render(request, template_name)
+        # TODO filterで公開非公開
+        novels = NOVEL.objects.all()
+        return render(request, self.template_name,{
+            "novels": novels,
+        })

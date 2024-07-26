322bea1634771e787bf0460235e7da3198c1d6f3
Mieda0310 <miedas@colabmix.co.jp>
2024-07-26 22:44:32 +0900

    トップページに背景画像を設定

--- src/myproject/myproject/views.py
+++ src/myproject/myproject/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Count
+
+from text_app.models import Novel
 
 class TopView(View):
     template_name = 'top.html'
     def get(self, request, *args, **kwargs):
+        last_three_novels = (Novel.objects.filter(is_public=True)
+                            .annotate(sentence_count=Count('sentence'))
+                            .order_by('-created_at')[:3])
         return render(request, self.template_name, {
-            
+            "last_three_novels": last_three_novels,
         })
